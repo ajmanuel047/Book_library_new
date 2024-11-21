22dc70e5bcdaf9780d65645c06ed502b27e0f741
Unuafe Ajiri <aj4short@rocketmail.com>
2024-11-21 01:11:27 +0100

    create an addBookToLibrary function to create the new book objects and add the objects to the myLibrary array

--- file.js
+++ file.js
@@ -1,6 +1,17 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;    
 }
+
+function addBookToLibrary(title, author, pages, read){
+    const book = new Book(title, author, pages, read)
+    myLibrary.push(book)
+}
+
+addBookToLibrary('harry', 'rowling', 345, 'yes')
+addBookToLibrary('Ring', 'me', 24, 'no')
+console.table(myLibrary)
