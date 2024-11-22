ec549c8d87a83e20a3d1748371cf284af698d810
Unuafe Ajiri <aj4short@rocketmail.com>
2024-11-23 00:50:07 +0100

    make the card container display the form when clicked

--- file.js
+++ file.js
@@ -1,12 +1,19 @@
 const myLibrary = [];
 const addBook = document.querySelector('.add_book > button');
 const form = document.querySelector('form')
+const cardContainer = document.querySelector('.cards_container')
+const containerTitle = document.querySelector('h3')
 
 addBook.addEventListener('click', function(){
     addBook.remove()
     form.style.display = 'flex'
 })
 
+cardContainer.addEventListener('click', function () {
+    containerTitle.remove()
+    addBook.remove()
+    form.style.display = 'flex'
+})
 
 function Book(title, author, pages, read){
     this.title = title;
